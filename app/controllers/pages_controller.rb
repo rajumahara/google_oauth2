35b427b90833d87a70e69c25976a761e656fc8f6
Rajendra Mahara <rajendra.mahara@aipan.tech>
2024-03-01 22:01:05 +0530

    #####

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  layout 'chatzone'
   def home
   end
 
--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,9 +4,22 @@ class SettingsController < ApplicationController
 
   def create 
     Rails.logger.info "test"
-    @conversation = Conversation.create!(created_by: current_user.user_id, conversation_type: "Single")
-    UserConversation.create(user_id: current_user.user_id, conversation_id: @conversation.conversation_id)
-    UserConversation.create(user_id: params[:user_id], conversation_id: @conversation.conversation_id)
+    existing_conversation = Conversation.joins(:user_conversations)
+      .where(user_conversations: { user_id: current_user.user_id })
+      .or(Conversation.joins(:user_conversations)
+          .where(user_conversations: { user_id: params[:user_id] }))
+      .where(conversation_type: "Single")
+      .group("conversations.conversation_id")
+      .having("COUNT(user_conversations.user_id) = 2")
+      .first
+
+    if existing_conversation.nil?
+      @conversation = Conversation.create!(created_by: current_user.user_id, conversation_type: "Single")
+      UserConversation.create(user_id: current_user.user_id, conversation_id: @conversation.conversation_id)
+      UserConversation.create(user_id: params[:user_id], conversation_id: @conversation.conversation_id)
+    else
+      @conversation = existing_conversation
+    end
     redirect_to message_path(@conversation)
   end
 end
